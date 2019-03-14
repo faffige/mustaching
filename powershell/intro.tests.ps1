f4cd7a74805906c46a67722f46e90142029f5000
faffige <>
2019-03-14 21:51:56 +1100

    fixing relativity and adding sections and lists

--- powershell/intro.tests.ps1
+++ powershell/intro.tests.ps1
@@ -1,27 +1,58 @@
-. .\intro.ps1
+. $PSScriptRoot\intro.ps1
 
 Describe 'Get-RenderedMustache' {
 
-    It 'basic render' {
-        $inputString = 'Hello {{person}}!'
-        $dictionary = @{
-            person = 'World'
+    Context 'variables' {
+
+        It 'basic render' {
+            $inputString = 'Hello {{person}}!'
+            $dictionary = @{
+                person = 'World'
+            }
+            Get-RenderedMustache -inputString $inputString -dictionary $dictionary |
+            Should be 'Hello World!'
         }
-        Get-RenderedMustache -inputString $inputString -dictionary $dictionary |
-        Should be 'Hello World!'
+
+        It 'complex render' {
+            $inputString = "@
+                Hello {{person}}, are you having a good {{expression}}?
+            @"
+
+            $dictionary = @{
+                person = 'John'
+                expression = 'day'
+            }
+
+            Get-RenderedMustache -inputString $inputString -dictionary $dictionary |
+            Should be "@
+                Hello John, are you having a good day?
+            @"
+        }
     }
 
-    It 'complex render' {
-        $inputString = "@
-            Hello {{person}}, are you having a good {{expression}}?
-        @"
-        $dictionary = @{
-            person = 'John'
-            expression = 'day'
+    Context 'sections' {
+
+        It 'basic list' {
+            $inputString = "@
+                Shown.
+                {{#person}}
+                    Never shown!
+                {{/person}}
+            @"
+
+            $dictionary = @{
+                person = $false
+            }
+
+            Get-RenderedMustache -inputString $inputString -dictionary $dictionary |
+            Should be "@
+                Shown.
+            @"
         }
-        Get-RenderedMustache -inputString $inputString -dictionary $dictionary |
-        Should be "@
-            Hello John, are you having a good day?
-        @"
     }
+    
+    Context 'Lists' {
+
+        
+    }   
 }
