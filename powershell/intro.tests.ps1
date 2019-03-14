8c8ed4a4c9a51f94d3b661b85f61b5a3171974f7
faffige <>
2019-03-14 21:35:31 +1100

    adding more tests

--- powershell/intro.tests.ps1
+++ powershell/intro.tests.ps1
@@ -10,4 +10,18 @@ Describe 'Get-RenderedMustache' {
         Get-RenderedMustache -inputString $inputString -dictionary $dictionary |
         Should be 'Hello World!'
     }
+
+    It 'complex render' {
+        $inputString = "@
+            Hello {{person}}, are you having a good {{expression}}?
+        @"
+        $dictionary = @{
+            person = 'John'
+            expression = 'day'
+        }
+        Get-RenderedMustache -inputString $inputString -dictionary $dictionary |
+        Should be "@
+            Hello John, are you having a good day?
+        @"
+    }
 }
